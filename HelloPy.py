a6c0c0674dd56386a36b60f1901cf079581b94cd
choiyj <astraltear@gmail.com>
2015-05-08 23:35:12 +0900

    by home

---
+++ HelloPy.py
@@ -0,0 +1,16 @@
+'''
+Created on 2015. 5. 8.
+
+@author: choiyj
+'''
+
+
+def greeting(hour,morning_msg="hello"):
+    if hour <9:
+        print morning_msg
+    elif hour < 12:
+        pass
+    
+
+
+greeting(7)
