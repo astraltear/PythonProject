77a1937fd1565c78406283ac1e529cba700fb191
yjchoi <yjchoi@strato.co.kr>
2023-08-08 11:12:56 +0900

    add

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# -*- coding: cp949 -*-
 
 # list append
 suitcase = [] 
@@ -10,8 +9,8 @@ suitcase.append("pants")
 
 list_length =len(suitcase)
 
-print "list length: %d" % (list_length)
-print suitcase
+print ("list length: %d" % (list_length))
+print (suitcase)
 
 # list string split
 suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
@@ -19,28 +18,28 @@ first = suitcase[0:2]
 middle = suitcase[2:4]
 last = suitcase[4:]
 
-print first
-print middle
-print last
+print (first)
+print (middle)
+print (last)
 
 animals = "catdogfrog"
 cat = animals[:3]
-print cat
+print (cat)
 
 dog = animals[3:6]
-print dog
+print (dog)
 
 frog = animals[6:]
-print frog
+print (frog)
 
 # list index insert
 animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
 
 duck_index= animals.index("duck")
-print duck_index
+print (duck_index)
 
 animals.insert(duck_index,"cobra")
-print animals
+print (animals)
 
 
 # list for loop
@@ -52,19 +51,19 @@ for number in start_list:
     
 
 square_list.sort()
-print square_list
+print (square_list)
 
 
 # dic add get del change
 residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
-print residents['Puffin']
-print residents['Sloth']
-print residents['Burmese Python']
+print (residents['Puffin'])
+print (residents['Sloth'])
+print (residents['Burmese Python'])
 
 
 menu={}
 menu['Chicken Alfredo'] = 14.50
-print menu['Chicken Alfredo']
+print (menu['Chicken Alfredo'])
 
 menu['a']=2
 menu['b']=3
@@ -82,8 +81,8 @@ del zoo_animals['Bengal Tiger']
 
 zoo_animals['Rockhopper Penguin']='Test'
 
-print zoo_animals
-print len(menu)
+print (zoo_animals)
+print (len(menu))
 
 # dic list change sort add
 
@@ -101,13 +100,13 @@ inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold']=inventory['gold']+50
 
-print inventory
+print (inventory)
 
 
 dic={"foo":"bar"}
 
 for key in dic:
-    print dic[key]
+    print (dic[key])
 
 # dic get key items()
 webster = {
@@ -118,17 +117,17 @@ webster = {
 }
 
 for web in webster:
-    print web
-    print webster[web]
+    print (web)
+    print (webster[web])
 
 for item in webster.items() :
-    print item
+    print (item)
 
 # control flow
 a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for index in a:
     if index %2 ==0:
-        print index
+        print (index)
 
 #list & function
 def fizz_count(x):
@@ -138,32 +137,32 @@ def fizz_count(x):
             cnt=cnt+1
     return cnt
     
-print fizz_count(['fizz','buzz','fizz'])
+print (fizz_count(['fizz','buzz','fizz']))
 
 n=[1,3,5]
 n[1]=n[1]*5
 n.append(4)
-print n
+print (n)
 
 
 def my_function(x):
     return x+3
 
 number=5
-print my_function(number)
+print (my_function(number))
 
 def list_function(nmlist):
     return nmlist[1]
 
 nm=[1,3,5]
-print list_function(nm)
+print( list_function(nm) )
 
 def list_extender(mylist):
     mylist[-1]=mylist[-1]+9
     return mylist
     
 nn = [3, 5, 7]
-print list_extender(nn)
+print( list_extender(nn) )
 
 def total(n):
     sum=0
@@ -171,7 +170,7 @@ def total(n):
         sum+= n[i]
     return sum
 
-print total(nn)
+print( total(nn))
 
 
 n = ["Michael", "Lieberman"]
@@ -182,7 +181,7 @@ def join_strings(n):
         str+=n[i]
     return str
 
-print join_strings(n)
+print( join_strings(n))
 
 m = [1, 2, 3]
 n = [4, 5, 6]
@@ -191,9 +190,9 @@ def join_lists(m,n):
     m.extend(n)
     return m
 
-print join_lists(m,n)
+print( join_lists(m,n))
 
-
+"""
 n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
 
 import types
@@ -201,29 +200,29 @@ def flatten(n):
     sumlist=[]
     for i in range(0,len(n)):
         if type(n[i]) == types.ListType:
-            print ">>"+str(len(n[i]))
+            print( ">>"+str(len(n[i])))
             for j in range(0,len(n[i])):
                 sumlist.append(n[i][j])
     return sumlist   
-print flatten(n)
-
+print( flatten(n))
+"""
 #Loop
 
 count =0
 
 if count < 5 :
-    print "if commnder", count
+    print( "if commnder", count)
 
 while count <= 9:
-    print "while commnder", count
+    print( "while commnder", count)
     count +=1
 
 num =1
 while num <=10:
-    print num**2
+    print( num**2)
     num+=1
 
-# input ÀÔ·ÂÀÌ ÀÖ¾î¼­ shell ÀÌ ¸ØÃá´Ù ±×·¡¼­ ÀÏ´Ü ÁÖ¼®
+# input ï¿½Ô·ï¿½ï¿½ï¿½ ï¿½Ö¾î¼­ shell ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½×·ï¿½ï¿½ï¿½ ï¿½Ï´ï¿½ ï¿½Ö¼ï¿½
 '''
 choice = raw_input("enjoying the course? (y/n)")
 while choice != 'y' and choice != 'n':
@@ -238,43 +237,43 @@ count = 0
 while count < 3:
     guess = raw_input("Enter a guess")
     if(guess == random_number):
-        print "You Win"
+        print( "You Win")
         break
     else:
         count+=1
 else:
-    print "You lose"
+    print( "You lose")
 '''
 
 s = "A bird in the hand..."
 
 for c in s:
     if c=='A' or c=='a':
-        print 'X',
+        print( 'X',)
     else:
-        print c,
-print
+        print( c,)
+print()
         
 d = {'x': 9, 'y': 10, 'z': 20}
 
 for key in d:
-    print key, d[key]
+    print( key, d[key])
 
 
 choices = ['pizza', 'pasta', 'salad', 'nachos']
 for index,item in enumerate(choices):
-    print index+1,item
+    print( index+1,item)
 
 list_a = [3, 9, 17, 15, 19]
 list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
 
-# ÇÔ¼ö zipÀº µÎ °³ÀÇ ¸®½ºÆ®¸¦ Àü´Ş¹Ş¾Æ ¿ä¼ÒÀÇ ½ÖÀ» »ı¼ºÇÏ°í,
-# ´õ ÂªÀº ÂÊÀÇ ¸®½ºÆ®ÀÇ ³¡ºÎºĞ¿¡¼­ »ı¼ºÀ» ¸ØÃä´Ï´Ù.
+# ï¿½Ô¼ï¿½ zipï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Æ®ï¿½ï¿½ ï¿½ï¿½ï¿½Ş¹Ş¾ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ï°ï¿½,
+# ï¿½ï¿½ Âªï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Æ®ï¿½ï¿½ ï¿½ï¿½ï¿½ÎºĞ¿ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ï´ï¿½.
 for a,b in zip(list_a,list_b):
     if a > b:
-        print a
+        print( a)
     else:
-        print b
+        print( b)
     
 
 
@@ -285,55 +284,55 @@ d = {
     "BDFL": True
 }
 
-print d.items()
+print( d.items())
 
-# ¸®½ºÆ® ³»Æ÷(list comprehension)
+# ï¿½ï¿½ï¿½ï¿½Æ® ï¿½ï¿½ï¿½ï¿½(list comprehension)
 evens_to_50 =[i for i in range(51) if i % 2 ==0]
-print evens_to_50
+print( evens_to_50)
 
-# 1ºÎÅÍ 5±îÁöÀÇ ¼øÂ÷Àû ¼ıÀÚ·Î ÀÌ·ç¾îÁø ¸®½ºÆ® new_list¸¦ »ı¼º
+# 1ï¿½ï¿½ï¿½ï¿½ 5ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ú·ï¿½ ï¿½Ì·ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Æ® new_listï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 new_list=[ x for x in range(1,6)]
-print new_list
+print( new_list)
 
-# ÀÌ ¼ıÀÚµéÀ» µÎ ¹è·Î ¸¸µé°í ½Í´Ù¸é, ´ÙÀ½°ú °°ÀÌ ÀÛ¼º
+# ï¿½ï¿½ ï¿½ï¿½ï¿½Úµï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Í´Ù¸ï¿½, ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Û¼ï¿½
 doubles = [x*2 for x in range(1,6) ]
-print doubles
+print( doubles)
 
-# ¸¸¾à ÀÌ·¸°Ô µÎ ¹è·Î ¸¸µç ¼ıÀÚ Áß¿¡¼­ 3À¸·Î ³ª´©¾î ¶³¾îÁö´Â ¼ıÀÚ¸¸À» ³²±â°í ½Í´Ù
+# ï¿½ï¿½ï¿½ï¿½ ï¿½Ì·ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ß¿ï¿½ï¿½ï¿½ 3ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ú¸ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Í´ï¿½
 doubles_by3 = [x*2 for x in range(1,6) if (x*2)%3==0 ]
-print doubles_by3
+print( doubles_by3)
 
 even_squares =[x**2 for x in range(1,11) if x%2==0]
-print even_squares
+print( even_squares)
 
 cubes_by_four=[ x for x in range(1,11) if (x**3)%4==0 ]
-print cubes_by_four
+print( cubes_by_four)
 
 my_list = range(1, 11)
 backwards = my_list[::-1]
-print backwards
+print( backwards)
 
 to_one_hundred = range(101)
 backwards_by_tens = to_one_hundred[::-10]
-print backwards_by_tens
+print( backwards_by_tens)
 
 to_21=range(1,22)
 odds=to_21[::2]
-print odds
+print( odds)
 
 middle_third= to_21[7:14]
-print middle_third
+print( middle_third)
 
 #lambda
 my_list = range(16)
-print filter(lambda x: x%3==0,my_list)
+print( filter(lambda x: x%3==0,my_list))
 
 languages = ["HTML", "JavaScript", "Python", "Ruby"]
-print filter(lambda x:x=='Python', languages)
+print( filter(lambda x:x=='Python', languages))
 
 squares=[x**2 for x in range(1,11)]
-print squares
-print filter(lambda x: x>=30 and x<70, squares)
+print( squares)
+print( filter(lambda x: x>=30 and x<70, squares))
 
 
 movies = {
@@ -342,29 +341,27 @@ movies = {
     "Monty Python's Meaning of Life": "Okay"
 }
 
-print movies.items()
+print( movies.items())
 for movie in movies.items():
-    print movie[0] ,":", movie[1]
+    print( movie[0] ,":", movie[1])
 
 
 threes_and_fives=[x for x in range(1,16) if x%3==0 or x%5==0]
-print threes_and_fives
+print( threes_and_fives)
 
 garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
 reverse_str = garbled[::-2]
-print reverse_str
+print( reverse_str)
 
 garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
 message=filter(lambda x: x!='X' ,garbled)
-print message
+print( message)
 
 
-# 2Áø¼ö
-print bin(1)
-print bin(2)
-print bin(3)
-print bin(4)
-print bin(5)
-print int( bin(0b1110 | 0b101) ,2)
-
-
+# 2ï¿½ï¿½ï¿½ï¿½
+print( bin(1))
+print( bin(2))
+print( bin(3))
+print( bin(4))
+print( bin(5))
+print( int( bin(0b1110 | 0b101) ,2))
